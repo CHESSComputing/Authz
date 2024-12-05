6679aa97a46bead73eb128337f551d4d30f3405d
Valentin Kuznetsov <vkuznet@gmail.com>
2024-12-05 12:55:12 -0500

    repace gorm with standard library sql module

--- users.go
+++ users.go
@@ -5,45 +5,72 @@ package main
 // Copyright (c) 2023 - Valentin Kuznetsov <vkuznet@gmail.com>
 //
 import (
+	"database/sql"
 	"errors"
 	"fmt"
 	"log"
-
-	srvConfig "github.com/CHESSComputing/golib/config"
-	"gorm.io/gorm"
+	"time"
 )
 
 // User represents user table
 type User struct {
-	ID         uint   `json:"id" gorm:"unsigned;primaryKey;autoIncrement:true,unique"`
+	ID         uint   `json:"id"`
 	LOGIN      string `json:"login"`
 	FIRST_NAME string `json:"first_name"`
 	LAST_NAME  string `json:"last_name"`
 	PASSWORD   string `json:"password"`
 	EMAIL      string `json:"email"`
-	UPDATED    int64  `json:"updated" gorm:"autoUpdateTime:milli"` // Use unix milli seconds as updating time
-	CREATED    int64  `json:"created" gorm:"autoCreateTime"`       // Use unix seconds as creating time
+	UPDATED    int64  `json:"updated"`
+	CREATED    int64  `json:"created"`
 }
 
-func getUser(db *gorm.DB, user User) (User, error) {
-	// Get first matched record
-	// SELECT * FROM users WHERE Login = ...
-	var u User
-	cond := fmt.Sprintf("LOGIN = ?")
-	result := db.Where(cond, user.LOGIN).First(&u)
-	//     result := db.First(&u, cond, user.Login)
-	if result.RowsAffected == 0 {
-		msg := fmt.Sprintf("User %s is not found", user.LOGIN)
+// getUser retrieves a user by their login from the database.
+func getUser(db *sql.DB, login string) (User, error) {
+	var user User
+	query := "SELECT id, login, first_name, last_name, password, email, updated, created FROM users WHERE login = ?"
+	row := db.QueryRow(query, login)
+
+	err := row.Scan(
+		&user.ID,
+		&user.LOGIN,
+		&user.FIRST_NAME,
+		&user.LAST_NAME,
+		&user.PASSWORD,
+		&user.EMAIL,
+		&user.UPDATED,
+		&user.CREATED)
+	if err == sql.ErrNoRows {
+		msg := fmt.Sprintf("User %s is not found", login)
 		log.Println("ERROR:", msg)
-		return u, errors.New(msg)
+		return user, errors.New(msg)
+	} else if err != nil {
+		log.Println("ERROR: failed to query user:", err)
+		return user, err
 	}
-	if srvConfig.Config.Authz.WebServer.Verbose > 0 {
-		log.Printf("INFO: query user %+v, result %+v, found %+v", user, result, u)
-	}
-	return u, nil
+
+	log.Printf("INFO: query user with login '%s', result %+v", login, user)
+	return user, nil
 }
 
-func createUser(db *gorm.DB, user User) (uint, error) {
-	result := db.Create(&user) // pass pointer of data to Create
-	return user.ID, result.Error
+// createUser inserts a new user into the database.
+func createUser(db *sql.DB, user User) (uint, error) {
+	query := `
+	INSERT INTO users (login, first_name, last_name, password, email, updated, created)
+	VALUES (?, ?, ?, ?, ?, ?, ?)
+	`
+	now := time.Now().UnixMilli()
+	result, err := db.Exec(query, user.LOGIN, user.FIRST_NAME, user.LAST_NAME, user.PASSWORD, user.EMAIL, now, now)
+	if err != nil {
+		log.Println("ERROR: failed to create user:", err)
+		return 0, err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		log.Println("ERROR: failed to retrieve last insert ID:", err)
+		return 0, err
+	}
+
+	log.Printf("INFO: created user with ID %d", id)
+	return uint(id), nil
 }
